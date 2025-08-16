46b62d7ef322f517cc1960cb0c81018caf5b8927
Thomas Froud <thomasfroud10@gmail.com>
2025-08-16 11:51:42 +0100

    Created basic models for agent & ticket

---
+++ Models/Ticket.cs
@@ -0,0 +1,12 @@
+﻿namespace BackOfficeTracker.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string? TicketNumber { get; set; }
+        public Agent? Agent { get; set; }
+        public string? Action { get; set; }
+        public string? Description { get; set; }
+        public int Time { get; set; }
+    }
+}
